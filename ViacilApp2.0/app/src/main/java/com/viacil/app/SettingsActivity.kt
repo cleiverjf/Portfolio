b116d6da2b2637f91e0472bfc345d17352afc996
cleiverjf <cleiverandresj@gmail.com>
2026-06-06 02:22:26 -0500

    Actualización de SettingsActivity con DataStore

--- ViacilApp2.0/app/src/main/java/com/viacil/app/SettingsActivity.kt
+++ ViacilApp2.0/app/src/main/java/com/viacil/app/SettingsActivity.kt
@@ -2,16 +2,36 @@ package com.viacil.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.viacil.app.databinding.ActivitySettingsBinding
+import com.viacil.app.utils.DataStoreManager
+import kotlinx.coroutines.launch
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
+    private lateinit var dataStoreManager: DataStoreManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // TODO: Implement accessibility options, DataStore, download languages
+        dataStoreManager = DataStoreManager(this)
+
+        // Load preferences
+        lifecycleScope.launch {
+            dataStoreManager.highContrast.collect { enabled ->
+                binding.switchHighContrast.isChecked = enabled
+            }
+        }
+
+        // Save on change
+        binding.switchHighContrast.setOnCheckedChangeListener { _, isChecked ->
+            lifecycleScope.launch {
+                dataStoreManager.setHighContrast(isChecked)
+            }
+        }
+
+        // Similar for other switches...
     }
 }
