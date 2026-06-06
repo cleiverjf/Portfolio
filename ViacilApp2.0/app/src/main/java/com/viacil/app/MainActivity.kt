1763b281b32fd409998ae5927d800b2f8cd40160
cleiverjf <cleiverandresj@gmail.com>
2026-06-06 01:51:26 -0500

    Add MainActivity, navigation and bottom navigation

--- ViacilApp2.0/app/src/main/java/com/viacil/app/MainActivity.kt
+++ ViacilApp2.0/app/src/main/java/com/viacil/app/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.viacil.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,13 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnCamera.setOnClickListener {
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, CameraActivity::class.java))
         }
-
         binding.btnInterpreter.setOnClickListener {
-            val intent = Intent(this, InterpreterActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, InterpreterActivity::class.java))
+        }
+        binding.btnTranslation.setOnClickListener {
+            startActivity(Intent(this, TranslationActivity::class.java))
+        }
+        binding.btnSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
