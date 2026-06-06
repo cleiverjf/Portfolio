b5b86ca23cf44fec753edf810364b5f77ee6d9eb
cleiverjf <cleiverandresj@gmail.com>
2026-06-06 02:31:58 -0500

    MainActivity actualizado con transiciones personalizadas por pantalla

--- ViacilApp2.0/app/src/main/java/com/viacil/app/MainActivity.kt
+++ ViacilApp2.0/app/src/main/java/com/viacil/app/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.viacil.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.viacil.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +13,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        binding.bottomNavigation.setupWithNavController(navController)
-
-        // Custom transitions
-        navController.addOnDestinationChangedListener { _, _, _ ->
-            overridePendingTransition(R.anim.scale_up, R.anim.slide_in_right)
+        binding.btnCamera.setOnClickListener {
+            startActivityWithTransition(CameraActivity::class.java, R.anim.rotate_scale, R.anim.explode_fly)
         }
+
+        binding.btnInterpreter.setOnClickListener {
+            startActivityWithTransition(InterpreterActivity::class.java, R.anim.fade_in_scale, R.anim.slide_up_fade)
+        }
+
+        binding.btnTranslation.setOnClickListener {
+            startActivityWithTransition(TranslationActivity::class.java, R.anim.scale_up, R.anim.slide_in_right)
+        }
+
+        binding.btnSettings.setOnClickListener {
+            startActivityWithTransition(SettingsActivity::class.java, R.anim.lens_focus, R.anim.fade_in_scale)
+        }
+    }
+
+    private fun startActivityWithTransition(activityClass: Class<*>, enterAnim: Int, exitAnim: Int) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+        overridePendingTransition(enterAnim, exitAnim)
     }
 }
